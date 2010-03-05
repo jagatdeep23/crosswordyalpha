29060ddb9cfc2c96ab2acda0ab200dafe257e059
matthewjmaze <matthewjmaze@c6c88ae0-0f60-11df-8179-3f26cfb4ba5f>
2010-03-05 23:27:30 +0000

    End Iteration 1

--- trunk/Yalpha/src/Controller/DriverGUI.java
+++ trunk/Yalpha/src/Controller/DriverGUI.java
@@ -1,8 +1,17 @@
 package Controller;
 
+/**
+ * Insertion point for the GUI.
+ *
+ * @author Team Yalpha, specifically Bob Grube and Matt Maze
+ * @version 1.0
+ */
 public class DriverGUI {
 
+    public DriverGUI(){
+        
+    }
 	public static void main(String[] args){
-		ControllerGUI controller = new ControllerGUI();
+		Controller controller = new Controller();
 	}
 }
